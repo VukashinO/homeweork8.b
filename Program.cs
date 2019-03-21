7f85003cec7dfa7388a7933533e6cc83c80c9f8e
Vukashin <vukashin@hotmail.com>
2019-03-21 02:40:18 +0100

    addedHomework

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework8.b
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enter name:");
+            var input = Console.ReadLine().ToLower();
+            var result = GetName(input);
+            Console.WriteLine($"{input} have phone number:{result}");
+        }
+
+        public static int GetName(string name)
+        {
+
+            Dictionary<string, int> phoneNumbers = new Dictionary<string, int>
+            {
+                { "vukashin", 70995605 },
+                { "ana", 70123456 },
+                { "viktor", 70654321 },
+                { "weko", 75123654 },
+                { "dejan", 70349420 }
+            };
+            if (phoneNumbers.ContainsKey(name))
+            {
+                return phoneNumbers[name];
+            }
+            throw new Exception($"there is no {name} name in our data base.");
+        }
+    }
+}
